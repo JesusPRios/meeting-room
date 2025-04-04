3c0e036f226bd202027a39a42d5206f1f153b850
Miguel Primera <141236101+JesusPRios@users.noreply.github.com>
2025-04-04 16:26:22 -0500

    Update PageBreadcrumb title in FormElements component to reflect reservation context

--- src/pages/Forms/FormElements.tsx
+++ src/pages/Forms/FormElements.tsx
@@ -4,7 +4,7 @@ import DefaultInputs from "../../components/form/form-elements/DefaultInputs";
 export default function FormElements() {
   return (
     <div>
-      <PageBreadcrumb pageTitle="Registro de sustancias" />
+      <PageBreadcrumb pageTitle="Registro de reserva" />
       <div className="grid grid-cols-1 gap-6 xl:grid-cols-2">
         <div className="space-y-6">
           <DefaultInputs />
