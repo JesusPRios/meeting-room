89904be3a426604278695bf2bb50981e14d98554
Miguel Primera <141236101+JesusPRios@users.noreply.github.com>
2025-04-07 16:16:01 -0500

    Refactor MetricsCharts component to display reservation information and integrate date selection

--- src/components/ecommerce/EcommerceMetrics.tsx
+++ src/components/ecommerce/EcommerceMetrics.tsx
@@ -1,105 +1,120 @@
-import { Bar } from "react-chartjs-2";
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-} from "chart.js";
-import Cookies from "js-cookie";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect } from "react";
+import { DayPicker } from "react-day-picker";
+import "react-day-picker/dist/style.css";
+import { useReservation } from "../../hooks/useReservation";
 import ComponentCard from "../common/ComponentCard";
-import { useCounts } from "../../hooks/useCount";
-
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend
-);
+import PageBreadcrumb from "../common/PageBreadCrumb";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHeader,
+  TableRow,
+} from "../../components/ui/table";
 
 export default function MetricsCharts() {
-  const { CountUsers, areaCount, sqCount } = useCounts();
-  const role = Cookies.get("role");
+  const {
+    selectedDate,
+    setSelectedDate,
+    getReservationByDate,
+    information,
+    formattedDate,
+    formatTime,
+  } = useReservation();
 
-  const labels = ["Muestras", "Áreas"];
-  const dataValues = [sqCount, areaCount];
+  useEffect(() => {
+    if (selectedDate) {
+      getReservationByDate(selectedDate);
+    }
+  }, [selectedDate]);
 
-  if (role === "admin") {
-    labels.push("Encargados");
-    dataValues.push(CountUsers);
-  }
+  return (
+    <>
+      <PageBreadcrumb pageTitle="Vista general de reservaciones" />
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        <div className="bg-white rounded-2xl shadow p-4">
+          <div className="scale-90 origin-top-left">
+            <DayPicker
+              mode="single"
+              selected={selectedDate}
+              onSelect={setSelectedDate}
+            />
+          </div>
+        </div>
 
-  const chartData = {
-    labels,
-    datasets: [
-      {
-        label: "Cantidad",
-        data: dataValues,
-        backgroundColor: [
-          "rgba(255, 159, 64, 0.7)",
-          "rgba(75, 192, 192, 0.7)",
-          "rgba(255, 99, 132, 0.7)",
-        ],
-        borderColor: [
-          "rgb(255, 159, 64)",
-          "rgb(75, 192, 192)",
-          "rgb(255, 99, 132)",
-        ],
-        borderWidth: 2,
-        borderRadius: 5,
-      },
-    ],
-  };
-
-  const chartOptions = {
-    responsive: true,
-    maintainAspectRatio: false,
-    scales: {
-      x: {
-        ticks: {
-          color: "gray",
-          font: {
-            size: 12,
-            weight: "bold" as const,
-          },
-        },
-      },
-      y: {
-        ticks: {
-          color: "gray",
-          font: {
-            size: 12,
-            weight: "bold" as const,
-          },
-        },
-      },
-    },
-    plugins: {
-      legend: {
-        position: "top" as const,
-        labels: {
-          color: "gray",
-          font: {
-            size: 11,
-            weight: "bold" as const,
-          },
-        },
-      },
-    },
-  };
-
-  // return (
-  //   // <ComponentCard title="Métricas de Gestión" className="w-11/12 mx-auto">
-  //   //   <p className="text-sm text-gray-500 dark:text-gray-400">
-  //   //     A continuación se muestran las cantidades de sustancias{role === "admin" ? ", encargados de las sustancias" : ""} y áreas registradas en el sistema.
-  //   //   </p>
-  //   //   <div className="relative h-80 w-full">
-  //   //   </div>
-  //   // </ComponentCard>
-
-  // );
+        <div className="col-span-2">
+          <ComponentCard
+            title={`Información de las reservaciones - ${
+              formattedDate ? formattedDate : ""
+            }`}
+          >
+            {selectedDate ? (
+              <div>
+                {information ? (
+                <Table>
+                <TableHeader className="border-b border-gray-100 dark:border-white/[0.05]">
+                  <TableRow>
+                    <TableCell
+                      isHeader
+                      className="px-5 py-3 font-semibold text-gray-500 text-start text-theme-sm dark:text-white"
+                    >
+                      User
+                    </TableCell>
+                    <TableCell
+                      isHeader
+                      className="px-5 py-3 font-semibold text-gray-500 text-start text-theme-sm dark:text-white"
+                    >
+                      Hora inicio
+                    </TableCell>
+                    <TableCell
+                      isHeader
+                      className="px-5 py-3 font-semibold text-gray-500 text-start text-theme-sm dark:text-white"
+                    >
+                      Hora fin
+                    </TableCell>
+                    <TableCell
+                      isHeader
+                      className="px-5 py-3 font-semibold text-gray-500 text-start text-theme-sm dark:text-white"
+                    >
+                      Estado
+                    </TableCell>
+                  </TableRow>
+                </TableHeader>
+              
+                <TableBody className="divide-y divide-gray-100 dark:divide-white/[0.05] text-sm">
+                  <TableRow>
+                    <TableCell className="sm:px-6 text-start text-gray-900 dark:text-white/90">
+                      {information.nombre_usuario}
+                    </TableCell>
+                    <TableCell className="sm:px-6 text-start text-gray-900 dark:text-white/90">
+                      {formatTime(information.timeStart)}
+                    </TableCell>
+                    <TableCell className="sm:px-6 text-start text-gray-900 dark:text-white/90">
+                      {formatTime(information.timeEnd)}
+                    </TableCell>
+                    <TableCell className="sm:px-6 text-start text-gray-900 dark:text-white/90">
+                      {information.status}
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+              
+               
+                ) : (
+                  <p className="text-sm text-gray-500">
+                    No hay reservas para esta fecha.
+                  </p>
+                )}
+              </div>
+            ) : (
+              <p className="text-sm text-gray-500">
+                Selecciona una fecha para ver información.
+              </p>
+            )}
+          </ComponentCard>
+        </div>
+      </div>
+    </>
+  );
 }
