9892a76f26b6ff1bc9016ee642ebd3ddcbd491c6
Miguel Primera <141236101+JesusPRios@users.noreply.github.com>
2025-04-07 16:16:10 -0500

    Refactor DefaultInputs component to integrate reservation handling and improve form structure

--- src/components/form/form-elements/DefaultInputs.tsx
+++ src/components/form/form-elements/DefaultInputs.tsx
@@ -1,63 +1,76 @@
 import ComponentCard from "../../common/ComponentCard";
 import Label from "../Label";
 import Input from "../input/InputField";
-import useSQData from "../../../hooks/useSQData";
 import Button from "../../ui/button/Button";
+import { useReservation } from "../../../hooks/useReservation";
 
 export default function DefaultInputs() {
   const {
-    RegisterSQ,
-    sq,
-    HandleNombreComercialChange,
-  } = useSQData();
+    registerReservation,
+    reser,
+    handleReasonChange,
+    handleDateChange,
+    handleTimeStartChange,
+    handleTimeEndChange,
+    handleDurationChange,
+    handleParticipantsChange,
+    handleCedulaUserChange,
+  } = useReservation();
+
 
   return (
-    <ComponentCard title="Reserva para sala de conferencia" desc="ak">
-      <form onSubmit={RegisterSQ}>
-        <div className="space-y-6">
-          <div>
+    <ComponentCard
+      title="Reserva para sala de conferencia"
+      desc="A continuación, encontrara el formulario correspondiente para registrar su reserva."
+    >
+      <form onSubmit={registerReservation}>
+        <div className="grid grid-cols-2 gap-4">
+          <div className="col-span-2">
             <Label>Razón o motivo de la reunión</Label>
-            <Input
+            <textarea
               required
-              type="text"
-              value={sq.nombre_comercial}
-              name="nombre_comercial"
-              autocomplete="off"
-              placeholder="Escriba el el motivo de la reunión"
-              onChange={HandleNombreComercialChange}
+              value={reser?.reason}
+              name="reason"
+              autoComplete="off"
+              placeholder="Escriba el motivo de la reunión"
+              onChange={handleReasonChange}
+              rows={5}
+              className="bg-transparent text-black border-gray-300 focus:border-brand-300 focus:ring focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:focus:border-brand-800 w-full rounded-lg border px-4 py-2.5 text-sm shadow-theme-xs focus:outline-none"
             />
           </div>
+
           <div>
             <Label>Fecha de la reunión</Label>
             <Input
               required
               type="date"
-              value={sq.nombre_comercial}
-              name="nombre_comercial"
+              value={reser?.date ? reser.date.toISOString().split("T")[0] : ""}
+              name="date"
               autocomplete="off"
-              onChange={HandleNombreComercialChange}
+              onChange={handleDateChange}
             />
           </div>
           <div>
             <Label>Hora de inicio</Label>
             <Input
               required
-              type="date"
-              value={sq.nombre_comercial}
-              name="nombre_comercial"
+              type="time"
+              value={reser?.timeStart}
+              name="timeStart"
               autocomplete="off"
-              onChange={HandleNombreComercialChange}
+              onChange={handleTimeStartChange}
             />
           </div>
+
           <div>
             <Label>Hora de fin</Label>
             <Input
               required
-              type="date"
-              value={sq.nombre_comercial}
-              name="nombre_comercial"
+              type="time"
+              value={reser?.timeEnd}
+              name="timeEnd"
               autocomplete="off"
-              onChange={HandleNombreComercialChange}
+              onChange={handleTimeEndChange}
             />
           </div>
           <div>
@@ -65,23 +78,24 @@ export default function DefaultInputs() {
             <Input
               required
               type="text"
-              value={sq.nombre_comercial}
-              name="nombre_comercial"
+              value={reser?.duration}
+              name="duration"
               autocomplete="off"
               readOnly={true}
-              onChange={HandleNombreComercialChange}
+              onChange={handleDurationChange}
             />
           </div>
+
           <div>
             <Label>Participantes</Label>
             <Input
               required
               type="text"
-              value={sq.nombre_comercial}
-              name="nombre_comercial"
+              value={reser?.participants}
+              name="participants"
               autocomplete="off"
-              readOnly={true}
-              onChange={HandleNombreComercialChange}
+              // readOnly={true}
+              onChange={handleParticipantsChange}
             />
           </div>
           <div>
@@ -89,44 +103,22 @@ export default function DefaultInputs() {
             <Input
               required
               type="text"
-              value={sq.nombre_comercial}
-              name="nombre_comercial"
+              value={reser?.cedula_user}
+              name="cedula_user"
               autocomplete="off"
-              readOnly={true}
+              // readOnly={true}
               placeholder="Ingrese su cédula"
-              onChange={HandleNombreComercialChange}
+              onChange={handleCedulaUserChange}
             />
           </div>
-          {/* <div>
-            <Label>Seleccione el área</Label>
-            <select
-              required
-              autoComplete="off"
-              className="h-11 w-full appearance-none rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 pr-11 text-sm shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-none focus:ring focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800"
-              onChange={HandleAreaChange}
-            >
-              <option value="">Seleccione una área</option>
-              {area.map((area) => (
-                <option key={area.id} value={area.id}>
-                  {area.name}
-                </option>
-              ))}
-            </select>
-          </div> */}
-          {/* <div>
-            <Label>Descripción de la sustancia</Label>
-            <textarea
-              value={sq.descripcion}
-              name="descripcion"
-              placeholder="Escriba una descripción"
-              className="bg-transparent text-black border-gray-300 focus:border-brand-300 focus:ring focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:focus:border-brand-80bg-transparent rk:focus:border-brand-80 w-full rounded-lg border px-4 py-2.5 text-sm shadow-theme-xs focus:outline-none"
-              rows={6}
-              onChange={HandleDescripcionChange}
-            />
-          </div> */}
         </div>
+
         <div className="flex justify-end">
-          <Button size="sm" type="submit" className="bg-[#39A900] hover:bg-[#39A900] w-full mt-3">
+          <Button
+            size="sm"
+            type="submit"
+            className="bg-[#39A900] hover:bg-[#39A900] w-full mt-3"
+          >
             Registrar
           </Button>
         </div>
