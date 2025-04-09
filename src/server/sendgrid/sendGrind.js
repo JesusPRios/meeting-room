48071dbff81a080147fcfc40bcc1f032343c74e0
Miguel Primera <141236101+JesusPRios@users.noreply.github.com>
2025-04-09 14:38:45 -0500

    Refactor SendGRID email logging for improved clarity and consistency

--- src/server/sendgrid/sendGrind.js
+++ src/server/sendgrid/sendGrind.js
@@ -3,29 +3,36 @@ import nodemailer from "nodemailer";
 const transporter = nodemailer.createTransport({
   host: "smtp.sendgrid.net",
   port: 587,
-  secure: false, 
+  secure: false,
   auth: {
-    user: "apikey", 
-    pass: "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", 
+    user: "apikey",
+    pass: "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",
   },
 });
 
 export const SendGRID = async ({ to, from, subject, text }) => {
+  console.log("Enviando correo a:", to);
+  console.log("Desde:", from);
+  console.log("Asunto:", subject);
+  console.log("Texto:", text);
+  console.log("Configuración del transportador:", transporter.options);
   try {
     const mailOptions = {
-      from, 
+      from,
       to,
       subject,
       text,
       priority: "high",
       headers: {
-        "Importance": "high", 
-        "X-Priority": "1", 
-        "X-MSMail-Priority": "High" 
-      }
+        Importance: "high",
+        "X-Priority": "1",
+        "X-MSMail-Priority": "High",
+      },
     };
 
     const info = await transporter.sendMail(mailOptions);
+    console.log("Mensaje enviado:", info);
+
     return info;
   } catch (error) {
     console.error("Error enviando el correo:", error);
