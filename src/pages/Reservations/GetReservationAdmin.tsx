00d22a58769c58ef549431b4cdbd38f4b737edfc
Miguel Primera <141236101+JesusPRios@users.noreply.github.com>
2025-04-22 11:29:23 -0500

    Refactor ReservationQuery component: update state management, enhance participant parsing, and improve time formatting

--- src/pages/Reservations/GetReservationAdmin.tsx
+++ src/pages/Reservations/GetReservationAdmin.tsx
@@ -15,48 +15,47 @@ import { useReservation } from "../../hooks/useReservation";
 
 const ReservationQuery = () => {
   const { id } = useParams();
-  const { information, getReservationById } = useReservation();
+  const {
+    reser,
+    getReservationById,
+    formatTimeTo12Hour,
+    capitalizeFirstLetter,
+  } = useReservation();
 
   useEffect(() => {
     if (id) getReservationById(Number(id));
   }, [id]);
 
-  if (!information) return <p className="text-gray-500">Cargando...</p>;
+  if (!reser) return <p className="text-gray-500">Cargando...</p>;
 
-  const participants = information.participants
-    ?.split(",")
-    .map((p) => p.trim());
+  const participants = reser.participants
+    ?.split(/,|\sy\s/)
+    .map((p) => capitalizeFirstLetter(p.trim()));
+  const formattedTimeStart = formatTimeTo12Hour(reser.timeStart);
+  const formattedTimeEnd = formatTimeTo12Hour(reser.timeEnd);
 
   return (
-    <ComponentCard title="Edición de la Reservación">
+    <ComponentCard title="Información de la reserva">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 text-sm mb-6">
         <div className="col-span-1 md:col-span-2">
-          <Info
-            icon={IoDocumentText}
-            label="Motivo"
-            value={information.reason}
-          />
+          <Info icon={IoDocumentText} label="Motivo" value={reser.reason} />
         </div>
 
         <Info
           icon={IoCalendar}
           label="Fecha"
-          value={new Date(information.date).toLocaleDateString()}
+          value={new Date(reser.date).toLocaleDateString()}
         />
         <Info
           icon={IoTime}
           label="Hora"
-          value={`${information.timeStart} - ${information.timeEnd}`}
+          value={`${formattedTimeStart} - ${formattedTimeEnd}`}
         />
-        <Info
-          icon={IoHourglass}
-          label="Duración"
-          value={information.duration}
-        />
+        <Info icon={IoHourglass} label="Duración" value={reser.duration} />
         <Info
           icon={IoPerson}
           label="Solicitante"
-          value={information.nombre_usuario}
+          value={reser.nombre_usuario}
         />
 
         <div className="col-span-1 md:col-span-2 p-4 rounded-lg bg-white dark:bg-gray-900 border dark:border-gray-700 shadow-sm">
@@ -78,18 +77,9 @@ const ReservationQuery = () => {
         </div>
 
         <div className="col-span-1 md:col-span-2">
-          <Info icon={IoPerson} label="Estado" value={information.status} />
+          <Info icon={IoPerson} label="Estado" value={reser.status} />
         </div>
       </div>
-
-      {/* <div className="flex justify-end gap-4">
-        <button className="bg-red-500 hover:bg-red-600 p-3 rounded-lg text-white text-sm">Cancelar</button>
-        <button className="bg-[#39A900] hover:bg-[#39A900] p-3 rounded-lg text-white text-sm"
-          onClick={() => AcceptReservation(information.id, information.cedula_user)}
-        >
-          Confirmar
-        </button>
-      </div> */}
     </ComponentCard>
   );
 };
